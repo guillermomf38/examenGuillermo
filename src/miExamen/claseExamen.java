4975e467cdc8893388fc853aaf9c401caad8ed6a
guillermomf38 <guillermomf38@educastur.es>
2024-02-23 19:25:33 +0200

    Añadidos comentarios de documentacion en claseExamen

--- src/miExamen/claseExamen.java
+++ src/miExamen/claseExamen.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package miExamen;
 
 /**
@@ -9,12 +6,22 @@ package miExamen;
  * @author alu14v
  */
 public class claseExamen {
+    /**
+     * Entornos de desarrollo UT3
+     * 
+     * Esta clase sirve para realizar el examen de la UT3 del módulo Entornos de Desarrollo
+     * Curso de Git y github de pildoras informaticas
+     *
+     *@author guillermo
+     * @version 1.0.0
+     */
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Estoy haciendo el examen de la UT3 ");
     }
     
 }
